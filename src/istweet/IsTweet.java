9d7e157083568ef0289a9effee429df53a3971ca
Adin Baskoro <master.adin@gmail.com>
2015-04-10 18:37:31 +0700

    First commit

---
+++ src/istweet/IsTweet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package istweet;
+import twitter4j.*;
+import java.util.List;
+import twitter4j.conf.ConfigurationBuilder;
+/**
+ *
+ * @author KEVIN
+ */
+public class IsTweet {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    ConfigurationBuilder cb = new ConfigurationBuilder(); 
+    cb.setDebugEnabled(true)
+            .setOAuthConsumerKey("2BDxsUOwujr6MwNArdW6T6l42")
+            .setOAuthConsumerSecret("RqgxWAZGFhcERbJcbp37p3Xa9nHh78qidTiW6cftqX46Taw1p3")
+            .setOAuthAccessToken("259316897-UKzBGelRixIeZYDtjHSDLoMjbib0Z57UOO4Sn9G5")
+            .setOAuthAccessTokenSecret("tPttYyNAVgjrSWAipw5FjdD9OntisyJrpJ79FeQHWv6bz");
+            // TODO code application logic here
+    
+    TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+ 
+
+ try {
+        Query query = new Query("ambis");
+        QueryResult result;
+        
+       do {
+            result = twitter.search(query);
+            List<Status> tweets = result.getTweets();
+            for (Status tweet : tweets) {
+                System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText() + tweet.getSource());
+            
+            }
+         } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        
+        }
+        }
+    }
+    
